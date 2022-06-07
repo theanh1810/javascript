9fedd2c4c3a92a05c3e409a30c3ad439c95566a6
ciddynguyen <theanh18102001@gmail.com>
2022-06-07 21:54:08 +0700

    upload soure code

---
+++ day10/script.js
@@ -0,0 +1,14 @@
+let fromC2F = function () {
+     let a = 20
+    let c= a * 1.8 + 32
+    console.log('Nhiet Do la ' + c + " " +"F")
+}
+fromC2F()
+
+let d = 5
+let b = 10
+let findArena = function (d, b) {
+    let S = d * b
+    console.log('Dien Tich HCN la' , S )
+}
+findArena(d,b)
