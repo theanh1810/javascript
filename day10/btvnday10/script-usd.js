39f66a8a6370ffb1247cf721458049a2c01ed97a
ciddynguyen <theanh18102001@gmail.com>
2022-06-08 12:14:24 +0700

    bt js

---
+++ day10/btvnday10/script-usd.js
@@ -0,0 +1,16 @@
+function USD2VND(m) {
+  const VND = m * 23500;
+  console.log(`${m}USD = ${VND}đ`);
+}
+
+USD2VND(4);
+USD2VND(2);
+function compoundInterest(P, r, n, t) {
+  const A = P * (1 + r / n) ** (n * t);
+  console.log(
+    `với số tiền ${P} sau ${t} năm gửi ngân hàng với lãi suất ${r} hàng năm thì thu được số tiền là ${A}đ `
+  );
+}
+compoundInterest(1000000, 0.6 / 100, 12, 2);
+compoundInterest(1000000, 0, 6 / 100, 4, 2);
+compoundInterest(1000000, 0, 6 / 100, 1, 2);
