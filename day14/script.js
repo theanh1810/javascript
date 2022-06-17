1272861ca4e7e60f2299e4f6f676a2cfad618c05
theanh1810 <72512268+theanh1810@users.noreply.github.com>
2022-06-17 15:46:13 +0700

    bai tap day14

---
+++ day14/script.js
@@ -0,0 +1,60 @@
+var dog = {
+    namE: "King of Dog",
+    age: 2,
+    bread: "Pit Pull",
+    gender: "Male",
+    color: "Black",
+    legs: 4,
+    favoriteFood: "Female Dogs",
+
+     eat() {
+    console.log(`${this.namE} is eating ${this.favoriteFood}, yum yum... `);
+    },
+    run() {
+    console.log(`${namE} is running on ${dog[legs]}...`)
+    },
+    bark() {
+    console.log("Woof Woof Woof")
+    }
+}
+console.log(dog)
+console.log(dog["namE"])
+console.log(dog["favoriteFood"])
+dog.color = "Brown"
+dog.age = dog.age + 2
+console.log(dog["age"])
+dog.eat();
+dog.run();
+dog.bark();
+for (let key in dog) {
+    console.log(`${key}`)
+}
+for (let key in dog) {
+  console.log(`${dog[key]}`);
+}
+dog.isCrazy = "max";
+dog.crazy=function(a){
+    if (a == dog.isCrazy) {
+        for (let key in dog) {
+            console.log(` ${dog.run()} ${dog.bark()} `);
+            
+      }
+    } else console.log(`${this.names} is not crazy`);
+  }
+
+dog.crazy("max")
+delete dog.isCrazy;
+dog.crazy("max")//vì đã xóa is crary nên giá trị chuyển về fale
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+console.log(key)
+let clone = dog;
+clone.names = "huy";
+console.log(clone);
+let copy = {};
+
+
