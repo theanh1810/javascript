08808edc96f1150b8601040203f289bdec452768
theanh1810 <72512268+theanh1810@users.noreply.github.com>
2022-06-17 16:02:14 +0700

    Update script.js

--- day14/script.js
+++ day14/script.js
@@ -1,57 +1,52 @@
 var dog = {
-    namE: "King of Dog",
-    age: 2,
-    bread: "Pit Pull",
-    gender: "Male",
-    color: "Black",
-    legs: 4,
-    favoriteFood: "Female Dogs",
-
-     eat() {
-    console.log(`${this.namE} is eating ${this.favoriteFood}, yum yum... `);
-    },
-    run() {
-    console.log(`${this.namE} is running on ${this.legs}...`)
-    },
-    bark() {
-    console.log("Woof Woof Woof")
-    }
-}
-console.log(dog)
-console.log(dog.namE)
-console.log(dog.favoriteFood)
-dog.color = "Brown"
-dog.age = dog.age + 2
-console.log(dog.age)
+  names: "King of Dogs",
+  age: 2,
+  bread: " Pit Pull",
+  gender: " Male",
+  color: " Black",
+  legs: 4,
+  favoritefood: "Female Dogs",
+  eat() {
+    console.log(`${this.names} is eating ${this.favoritefood}; `);
+  },
+  run() {
+    console.log(`${this.names} is running on ${this.legs}; `);
+  },
+  bark() {
+    console.log(" Woof woof woof ...");
+  },
+};
+console.log(dog);
+console.log(dog.names);
+console.log(dog.favoritefood);
+dog.color = " brown";
 dog.eat();
 dog.run();
 dog.bark();
 for (let key in dog) {
-    console.log(`${key}`)
+  console.log(`${key}`);
 }
 for (let key in dog) {
   console.log(`${dog[key]}`);
 }
 dog.isCrazy = "max";
-dog.crazy=function(a){
-    if (a == dog.isCrazy) {
-        for (let key in dog) {
-            console.log(` ${dog.run()} ${dog.bark()} `);
-            
-      }
-    } else console.log(`${this.namE} is not crazy`);
-  }
-
-dog.crazy("max")
-delete dog.isCrazy;
-dog.crazy("max")//vì đã xóa is crary nên giá trị chuyển về fale
-for (let key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        
+dog.crazy = function (a) {
+  if (a == dog.isCrazy) {
+    for (let key in dog) {
+      console.log(` ${dog.run()} ${dog.bark()} `);
     }
+  } else console.log(`${this.names} is not crazy`);
+};
+
+dog.crazy("max");
+delete dog.isCrazy;
+dog.crazy("max"); //vì đã xóa is crary nên giá trị chuyển về fale
+for (const key in object) {
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+  }
 }
-console.log(key)
+console.log(key);
 let clone = dog;
 clone.namE = "CoCa";
 console.log(clone);
